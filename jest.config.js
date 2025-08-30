68d7e9bf5157d784010a9b0fa3bfcc57fbe8cb80
stalkrbeardedler18 <rumlerguoyi@hotmail.com>
2025-08-30 00:24:49 -0500

    Add comments for clarity in Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 // Jest configuration file
+// Specifies the testing environment as Node.js
 module.exports = {
   testEnvironment: 'node',
+  // Use babel-jest for transforming JavaScript files
   transform: {
     '^.+\.js$': 'babel-jest',
   },
-  testRegex: '(/__tests__/.*|(\.|/)(test|spec))\.js$'
+  // Regex pattern to identify test files
+  testRegex: '(/__tests__/.*|(.|/)(test|spec))\.js$'
 };
